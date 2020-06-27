4cf3eb387433166b68df969f8e09339a51bc3f35
yamaan93 <yamaan93@gmail.com>
2020-06-27 03:33:56 -0400

    idk man, im stuck on why interpolation didnt work

---
+++ Assets/Scripts/weaponMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class weaponMovement : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+       transform.localPosition = new Vector3(0f, Mathf.Lerp(3f, 5f, 0.000005f));
+      
+    }
+
+    void Update()
+    {
+        Debug.Log(Mathf.Lerp(3f, 5f, 0.00000005f* Time.deltaTime));
+    }
+}
