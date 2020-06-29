6c167dca0b5eb2426833e9871cedc8106bd83356
yamaan93 <yamaan93@gmail.com>
2020-06-28 22:03:02 -0400

    Lerp work :)

--- Assets/Scripts/weaponMovement.cs
+++ Assets/Scripts/weaponMovement.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class weaponMovement : MonoBehaviour
 {
-    
-    
+
+    public float t = 0.0f;//interpolation value 
+    private float weaponX;
+    private float weaponY;
     void Start()
     {
         
@@ -16,13 +19,17 @@ public class weaponMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        t+= 0.01f;
+        weaponY = Mathf.Lerp(-0.04142f, 0.2414f, t);
+        weaponX = math.sqrt(0.02f - math.pow(weaponY - 0.1f, 2)) / 1f;
+        
 
-       transform.localPosition = new Vector3(0f, Mathf.Lerp(3f, 5f, 0.000005f));
+        transform.localPosition = new Vector3(weaponX, weaponY);
       
     }
 
     void Update()
     {
-        Debug.Log(Mathf.Lerp(3f, 5f, 0.00000005f* Time.deltaTime));
+        
     }
 }
